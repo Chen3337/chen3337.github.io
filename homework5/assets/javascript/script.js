6f7168060aa93e37e4f540bda4aca874cd3b78e8
zhen yong chen <dannychen111@yahoo.com>
2019-10-17 23:25:26 -0500

    add in the js file a function that make the hours to be past present and future.

--- homework5/assets/javascript/script.js
+++ homework5/assets/javascript/script.js
@@ -1,1 +1,25 @@
 $("#currentDay").text(moment().format('LL'));
+var hourOfday = moment().format('h');
+hourOfday = parseInt(hourOfday);
+var amOrpm = moment().format('a');
+if(amOrpm === "pm"){
+    hourOfday = hourOfday + 12;
+}
+var theIDs = [9,10,11,12,13,14,15,16,17];
+colorTheHours();
+function colorTheHours(){
+    for(i=0; i < theIDs.length; i++){
+        if(theIDs[i] < hourOfday){
+            $("#" + theIDs[i]).addClass("past");
+        }
+        else if(theIDs[i] > hourOfday){
+            $("#" + theIDs[i]).addClass("future");
+        }
+        else{
+            $("#" + theIDs[i]).addClass("present");
+        }
+
+    }
+    
+}
+
