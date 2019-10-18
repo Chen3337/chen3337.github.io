fd297dfe6a24f08088c74f406f9226e862f748fa
zhen yong chen <dannychen111@yahoo.com>
2019-10-18 00:58:47 -0500

    hoursofday a little change to make it work better at 12 clock and added savebtn onclick event also able to console log the text entered

--- homework5/assets/javascript/script.js
+++ homework5/assets/javascript/script.js
@@ -4,8 +4,27 @@ hourOfday = parseInt(hourOfday);
 var amOrpm = moment().format('a');
 if(amOrpm === "pm"){
     hourOfday = hourOfday + 12;
+    if(hourOfday === 24){
+        hourOfday = 12;
+    }
+}
+else if(hourOfday === 12){
+    hourOfday = 0;
 }
 var theIDs = [9,10,11,12,13,14,15,16,17];
+var thestorage = {
+    theHoursText : [{
+        9 : ""},
+        {10 : ""},
+        {11 : ""},
+        {12 : ""},
+        {13 : ""},
+        {14 : ""},
+        {15 : ""},
+        {16 : ""},
+        {17 : ""
+        }]
+}
 colorTheHours();
 function colorTheHours(){
     for(i=0; i < theIDs.length; i++){
@@ -18,8 +37,14 @@ function colorTheHours(){
         else{
             $("#" + theIDs[i]).addClass("present");
         }
-
     }
-    
 }
+$(".saveBtn").on("click", function(event){
+    event.stopPropagation();
+    var theID = event.target.parentElement.children[1].id;
+    var theTextAreaValue = $("#" + theID).children().val();
+    console.log(theTextAreaValue);
+    
+
+});
 
