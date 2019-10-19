42e1fef14cad3ed4238fb2d474403eac08c75040
zhen yong chen <dannychen111@yahoo.com>
2019-10-18 23:41:13 -0500

    now it will change the color of the time block of the hour changes added the timer to check it every minuts

--- homework5/assets/javascript/script.js
+++ homework5/assets/javascript/script.js
@@ -1,30 +1,12 @@
+// this gets the day
 $("#currentDay").text(moment().format('LL'));
-var hourOfday = moment().format('h');
-hourOfday = parseInt(hourOfday);
-var amOrpm = moment().format('a');
-if(amOrpm === "pm"){
-    hourOfday = hourOfday + 12;
-    if(hourOfday === 24){
-        hourOfday = 12;
-    }
-}
-else if(hourOfday === 12){
-    hourOfday = 0;
-}
+// the ids of the divs
 var theIDs = [9,10,11,12,13,14,15,16,17];
+// the localstorage object
 var thestorage = {
-    theHoursText : [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        ""
-        ]
+    theHoursText : ["","","","","","","","",""]
 }
+// gets information from the localstorage if there is any
 if(JSON.parse(localStorage.getItem("thestorage")) === null){
 }
 else{
@@ -32,18 +14,17 @@ else{
     for(i=0; i < thestorage.theHoursText.length;i++){
         var thisNumber = i + 9;
         var theTextBox = $("#" + thisNumber).children();
-        
         theTextBox.text(thestorage.theHoursText[i]);
     }
 }
-
-colorTheHours();
+// this will find out if in past present or the future and add the class to change the color
 function colorTheHours(){
+    var theHourTime = theHour();
     for(i=0; i < theIDs.length; i++){
-        if(theIDs[i] < hourOfday){
+        if(theIDs[i] < theHourTime){
             $("#" + theIDs[i]).addClass("past");
         }
-        else if(theIDs[i] > hourOfday){
+        else if(theIDs[i] > theHourTime){
             $("#" + theIDs[i]).addClass("future");
         }
         else{
@@ -51,6 +32,23 @@ function colorTheHours(){
         }
     }
 }
+// get the hour of the day by 24 hours
+function theHour(){
+    var hourOfday = moment().format('h');
+    hourOfday = parseInt(hourOfday);
+    var amOrpm = moment().format('a');
+    if(amOrpm === "pm"){
+        hourOfday = hourOfday + 12;
+        if(hourOfday === 24){
+            hourOfday = 12;
+        }
+    }
+    else if(hourOfday === 12){
+        hourOfday = 0;
+    }
+    return hourOfday;
+}
+// when click on it will save it to the localstorage so it will not be lose
 $(".saveBtn").on("click", function(event){
     event.stopPropagation();
     var theID = event.target.parentElement.children[1].id;
@@ -58,4 +56,7 @@ $(".saveBtn").on("click", function(event){
     thestorage.theHoursText[(theID - 9)] = theTextAreaValue;
     localStorage.setItem("thestorage", JSON.stringify(thestorage));
 });
+colorTheHours();
+// checks the hour and change if needed
+var thetimeinterval = setInterval(colorTheHours,60000);
 
